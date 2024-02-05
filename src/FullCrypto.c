a08e1140bde34706f299a3ddfbbd6e0e1aa7abe5
Kelpering <tristan8272@gmail.com>
2024-02-05 10:19:03 -0700

    Create FullCrypto.c

---
+++ src/FullCrypto.c
@@ -0,0 +1,20 @@
+#include "../include/FullCrypto.h"
+
+// ECB AES256
+
+// CBC AES256
+
+uint8_t* ECBAESEnc(uint8_t* Plaintext, uint8_t* Key)
+{
+    // Pad data with the number equal to pad bytes (minimum 1, maximum 16)
+    // Copy to new buffer. Recommend in documentation to free Plaintext.
+    // Loop through all
+    
+    return NULL;
+}
+
+uint8_t* ECBAESDec(uint8_t* Ciphertext, uint8_t* Key)
+{
+    // a
+    return NULL;
+}
