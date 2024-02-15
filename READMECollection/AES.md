999efc0fabc9bcbddcee225a54f73c42183ca19f
Kelpering <tristan8272@gmail.com>
2024-02-15 09:44:13 -0700

    Moved template CBC code

--- READMECollection/AES.md
+++ READMECollection/AES.md
@@ -71,3 +71,24 @@ int main()
     return 0;
 }
 ```
+
+## AES Continued
+
+```C
+    // Shows some code for AES CBC (similar to ECB) for future integration.
+    uint8_t Data[] = "HELLO WORLD!";
+    uint8_t* IV = IVGen(time(NULL));
+    uint8_t* Key = AESKeyGen256(time(NULL));
+
+    PrintInfo(Data, sizeof(Data), true);
+
+    ByteArr EncData = CBCAESEnc(Data, sizeof(Data), Key, IV);
+    PrintInfo(EncData.Arr, EncData.Size, false);
+    
+    ByteArr DecData = CBCAESDec(EncData.Arr, EncData.Size, Key, IV);
+    PrintInfo(DecData.Arr, DecData.Size, true);
+
+    free(EncData.Arr);
+    free(DecData.Arr);
+
+```
--- src/Main.c
+++ src/Main.c
@@ -12,21 +12,7 @@ void PrintInfo(uint8_t* Array, size_t Size, bool isString);
 
 int main()
 {
-    char HashString[33];
-    uint8_t Data[] = "HELLO WORLD!";
-    uint8_t* IV = IVGen(time(NULL));
-    uint8_t* Key = AESKeyGen256(time(NULL));
-
-    PrintInfo(Data, sizeof(Data), true);
-
-    ByteArr EncData = CBCAESEnc(Data, sizeof(Data), Key, IV);
-    PrintInfo(EncData.Arr, EncData.Size, false);
-    
-    ByteArr DecData = CBCAESDec(EncData.Arr, EncData.Size, Key, IV);
-    PrintInfo(DecData.Arr, DecData.Size, true);
-
-    free(EncData.Arr);
-    free(DecData.Arr);
+    // Main serves as a playground for now.
     return 0;
 }
 
